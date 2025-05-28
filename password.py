dce170a0756661726c8f24919a066c75cd33446e
Rickard <bv.rickard.stahl@gmail.com>
2025-05-28 21:04:59 +0200

    main

--- password.py
+++ password.py
@@ -8,6 +8,7 @@ pygame.init()
 WIDTH, HEIGHT = 1366, 768
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BookBeat")
+
 # Load images
 background2 = pygame.image.load("images/background2.png")
 vector_image = pygame.image.load("images/vector2.png")
@@ -18,7 +19,6 @@ mail_icon = pygame.image.load("images/mail.png")
 btn2_img = pygame.image.load("images/btn2.png")
 btnR_img = pygame.image.load("images/backgroundReg.png")
 pygame.display.set_icon(bookbeat_icon)
-
 
 # Fonts
 FONT = pygame.font.SysFont("Zapf Renaissance Book", 20, bold=True)
@@ -51,12 +51,17 @@ active_field = None
 feedback = ""
 is_register_screen = False
 
-# Rectangles
+# Rectangles (login screen)
 username_rect = pygame.Rect(550, 330, 300, 35)
 password_rect = pygame.Rect(550, 410, 300, 35)
 login_button_rect = pygame.Rect(550, 470, 250, 45)
 top_register_button_rect = pygame.Rect(WIDTH - 190, 10, 120, 40)
-email_rect = pygame.Rect(550, 490, 300, 35)
+
+# Rectangles (register screen)
+email_rect = pygame.Rect(550, 490, 250, 35)
+username_rect_reg = pygame.Rect(550, 330, 250, 35)
+password_rect_reg = pygame.Rect(550, 410, 250, 35)
+skapa_konto_rect = pygame.Rect(550, 550, 250, 45)
 
 def draw_text(text, pos, font=FONTSMALL, color=WHITE):
     label = font.render(text, True, color)
@@ -90,48 +95,63 @@ while running:
             running = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Mouse click at:", event.pos)
             if not is_register_screen:
                 if username_rect.collidepoint(event.pos):
-                    print("Username clicked")
                     active_field = "username"
                 elif password_rect.collidepoint(event.pos):
-                    print("Password clicked")
                     active_field = "password"
                 elif login_button_rect.collidepoint(event.pos):
-                    print("Login button clicked")
                     check_login()
                 elif top_register_button_rect.collidepoint(event.pos):
-                    print("Register screen")
                     is_register_screen = True
+                    username_input = ""
+                    password_input = ""
+                    email_input = ""
+                    feedback = ""
                 else:
                     active_field = None
             else:
-                if username_rect.collidepoint(event.pos):
+                # Register screen mouse clicks
+                if username_rect_reg.collidepoint(event.pos):
                     active_field = "username"
-                elif password_rect.collidepoint(event.pos):
+                elif password_rect_reg.collidepoint(event.pos):
                     active_field = "password"
                 elif email_rect.collidepoint(event.pos):
                     active_field = "email"
+                elif skapa_konto_rect.collidepoint(event.pos):
+                    # Register user
+                    if username_input and password_input and email_input:
+                        if username_input in users_dict:
+                            feedback = "Användarnamnet är redan taget."
+                        else:
+                            users_dict[username_input] = password_input
+                            with open(users_file, "w") as f:
+                                json.dump(users_dict, f)
+                            feedback = f"User {username_input} registered successfully."
+                            username_input = ""
+                            password_input = ""
+                            email_input = ""
+                            is_register_screen = False
+                    else:
+                        feedback = "Alla fält måste fyllas i."
                 else:
                     active_field = None
 
         elif event.type == pygame.KEYDOWN:
-            
             if active_field == "username":
                 if event.key == pygame.K_BACKSPACE:
                     username_input = username_input[:-1]
-                else:
+                elif len(event.unicode) == 1:
                     username_input += event.unicode
             elif active_field == "password":
                 if event.key == pygame.K_BACKSPACE:
                     password_input = password_input[:-1]
-                else:
+                elif len(event.unicode) == 1:
                     password_input += event.unicode
             elif active_field == "email":
                 if event.key == pygame.K_BACKSPACE:
                     email_input = email_input[:-1]
-                else:
+                elif len(event.unicode) == 1:
                     email_input += event.unicode
 
     # Draw interface
@@ -142,27 +162,29 @@ while running:
         headline_label = BIG_FONT.render("Kom igång med ett konto", True, BLACK)
         screen.blit(headline_label, headline_label.get_rect(center=(WIDTH // 2, 200)))
 
-        draw_text("Användarnamn", (username_rect.x, username_rect.y - 30), FONTSMALL, BLACK)
-        draw_text("Lösenord", (password_rect.x, password_rect.y - 30), FONTSMALL, BLACK)
+        pygame.draw.rect(screen, GRAY, username_rect_reg, 2)
+        pygame.draw.rect(screen, GRAY, password_rect_reg, 2)
+        pygame.draw.rect(screen, GRAY, email_rect, 2)
+        pygame.draw.rect(screen, PURPLE, skapa_konto_rect)
+
+        draw_text("Användarnamn", (username_rect_reg.x, username_rect_reg.y - 30), FONTSMALL, BLACK)
+        draw_text("Lösenord", (password_rect_reg.x, password_rect_reg.y - 30), FONTSMALL, BLACK)
         draw_text("Mailadress", (email_rect.x, email_rect.y - 30), FONTSMALL, BLACK)
+        draw_text("Skapa konto", (skapa_konto_rect.x + 80, skapa_konto_rect.y + 14), FONTSMALL, BLACK)
 
         screen.blit(username_icon, (520, 335))
         screen.blit(password_icon, (520, 415))
         screen.blit(mail_icon, (520, 495))
 
-        pygame.draw.rect(screen, GRAY, username_rect, 2)
-        pygame.draw.rect(screen, GRAY, password_rect, 2)
-        pygame.draw.rect(screen, GRAY, email_rect, 2)
-
-        draw_text(username_input or "Skriv användarnamn", (username_rect.x + 10, username_rect.y + 10), FONTARIAL, BLACK if username_input else (180, 180, 180))
-        draw_text("*" * len(password_input) if password_input else "Skriv lösenord", (password_rect.x + 10, password_rect.y + 10), FONTARIAL, BLACK if password_input else (180, 180, 180))
+        draw_text(username_input or "Skriv användarnamn", (username_rect_reg.x + 10, username_rect_reg.y + 10), FONTARIAL, BLACK if username_input else (180, 180, 180))
+        draw_text("*" * len(password_input) if password_input else "Skriv lösenord", (password_rect_reg.x + 10, password_rect_reg.y + 10), FONTARIAL, BLACK if password_input else (180, 180, 180))
         draw_text(email_input or "Skriv mailadress", (email_rect.x + 10, email_rect.y + 10), FONTARIAL, BLACK if email_input else (180, 180, 180))
 
         # Cursor rendering
         if active_field in ["username", "password", "email"] and cursor_visible:
             input_map = {
-                "username": (username_input, username_rect),
-                "password": ("*" * len(password_input), password_rect),
+                "username": (username_input, username_rect_reg),
+                "password": ("*" * len(password_input), password_rect_reg),
                 "email": (email_input, email_rect),
             }
             text_val, rect = input_map[active_field]
@@ -207,3 +229,5 @@ while running:
         screen.blit(feedback_label, feedback_label.get_rect(center=(login_button_rect.centerx, 640)))
 
     pygame.display.update()
+
+pygame.quit()
